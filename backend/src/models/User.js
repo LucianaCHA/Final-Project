36ecaafe85af4c4734afb42bf9db8c4f5df79b62
DESKTOP-30KTMEV\Equipo <japr0990@gmail.com>
2022-04-26 14:06:50 -0300

    modelo_User

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,12 +1,41 @@
 const { DataTypes } = require('sequelize');
+const { mapWhereFieldNames } = require('sequelize/types/utils');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('user', {
+    ID:{
+      type:DataTypes.UUID,
+      defaultValue:DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    surname:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    usuario:{
+     type:DataTypes.STRING,
+      allowNull:false
+    },
+    pass:{
+      type:DataTypes.STRING,
+      allowNull:false 
+    },
+    /*subscription:{
+      type: datatypes.UUID
+      allowNull:true
+    },*/
+    favorite:{
+      type:DataTypes.ARRAY(DataTypes.UUID)
+    }
+
+
   });
 };
+
+
