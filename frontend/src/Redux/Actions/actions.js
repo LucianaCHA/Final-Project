d171d8b667ad11d8eb13b7e721be6d8095e57544
MelisaF <ferreyramelisa91@gmail.com>
2022-04-26 18:31:20 -0300

    first actions

--- frontend/src/Redux/Actions/actions.js
+++ frontend/src/Redux/Actions/actions.js
@@ -1,1 +1,16 @@
 import axios from 'axios'; 
+
+export function getAllApi() {
+    return async function(dispatch) {
+        try {
+            const apiInfo = await axios.get('localhost:3001/characters');
+            return dispatch({
+                type:'GET_CHARACTERS',
+                payload: apiInfo.data
+            })
+        }
+        catch(err) {
+            console.log(err)
+        }
+    }
+}
