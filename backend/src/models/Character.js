5c1d52349a041cd6d417bc720fc44332d01b37d8
DESKTOP-30KTMEV\Equipo <japr0990@gmail.com>
2022-04-26 11:29:47 -0300

    modelado de la tabla de personajes/Characters

---
+++ backend/src/models/Character.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+const { mapWhereFieldNames } = require('sequelize/types/utils');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('comic', {
+    id:{
+      type:DataTypes.UUID,
+      defaultValue:DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
+  
